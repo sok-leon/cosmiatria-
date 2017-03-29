8774d87e22936b53760e3a049def312ed530768d
MonserMoreno <monsecarvajalm@gmail.com>
2017-03-29 16:40:46 -0600

    servicios funciona

--- resources/views/cosmiatria/servicios.blade.php
+++ resources/views/cosmiatria/servicios.blade.php
@@ -1,15 +1,13 @@
 @extends('layout')
 @section('servicios')
 
-@set('i', 0);
+<?php $i = 0; ?>
 @foreach ($servicios as $servicios)
-      <hr class="featurette-divider">
+  @if($i%2==0)
+
+    <hr class="featurette-divider">
       <div class="row featurette">
-    @if(i%2==0)
-      <div class="col-md-7">
-      else
-      <div class="col-md-7 col-md-push-5">
-    @endif
+        <div class="col-md-7">
           <h2 class="featurette-heading">{{ $servicios->nombre}} <span class="text-muted"></span></h2>
           <p class="lead">{{ $servicios->descripcion}}</p>
         </div>
@@ -18,11 +16,22 @@
         </div>
       </div>
 
-      <hr class="featurette-divider">
 
-      $i++;
+@else
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">{{ $servicios->nombre}} <span class="text-muted"></span></h2>
+          <p class="lead">{{ $servicios->descripcion}}</p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+@endif
+<?php $i++; ?>
 @endforeach
-      <hr class="featurette-divider">
+
 
 
 @endsection
