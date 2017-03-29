e265f0cfd642270a548ad8742477582b3b4920af
MonserMoreno <monsecarvajalm@gmail.com>
2017-03-29 16:21:55 -0600

    no funciona servicios, plantilla nueva

--- resources/views/cosmiatria/servicios.blade.php
+++ resources/views/cosmiatria/servicios.blade.php
@@ -1,14 +1,28 @@
 @extends('layout')
-@section('contenido')
-<h1> Servicios </h1><br>
-<ul>
-  @foreach ($servicios as $servicios)
-    <li>
-        <h2><br>
-          {{ $servicios->nombre}}
-        </h2>
-        <p>{{ $servicios->descripcion}}</p>
-    </li>
-  @endforeach
-</ul>
+@section('servicios')
+
+@set('i', 0);
+@foreach ($servicios as $servicios)
+      <hr class="featurette-divider">
+      <div class="row featurette">
+    @if(i%2==0)
+      <div class="col-md-7">
+      else
+      <div class="col-md-7 col-md-push-5">
+    @endif
+          <h2 class="featurette-heading">{{ $servicios->nombre}} <span class="text-muted"></span></h2>
+          <p class="lead">{{ $servicios->descripcion}}</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      $i++;
+@endforeach
+      <hr class="featurette-divider">
+
+
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,95 +1,97 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Cosmiatría</title>
-<link href="assets/css/styles.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" type="text/css" href="assets/css/styles2.css">
-<!--prueba 1-->
-</head>
-<body>
-<div class="menu-wrapper">
-  <div class="menu">
-    <ul>
-      <li><a href="index.php" class="active">Inicio</a></li>
-      <li><a href="servicios.php">Servicios</a></li>
-      <li><a href="preguntas.php">Preguntas</a></li>
-    </ul>
-  </div>
-  <div class="call">
-    <h1>Tel. :<span> TELEFONO </span></h1>
-  </div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Carousel Template for Bootstrap</title>
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Cosmiatría</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li><a href="/servicios">Sercivios</a></li>
+                <li><a href="/preguntas">Preguntas Frecuantes</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+    <!-- Carousel
+    ================================================== -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+@yield('carrusel')
 </div>
-<!--- menu-wrapper div end -->
-<div class="clearing"></div>
-<div class="border-bottom"></div>
-<div class="logo-wrapper">
-  <!--div class="leftshadow"><img src="images/logo-wrap-left.jpg" /></div-->
-  <div class="logo">
-    <h1>Elizabeth Parra</h1>
-    <h2 align="right" style="width: 200px;"> Cosmetóloga y cosmiatra</h2>
-  </div>
 
-</div>
-<div class="clearing"></div>
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
 
-<div class ="banner-wrapper">
-  @yield('slider')
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        @yield('contenido')
+      </div><!-- /.row -->
 
 
-</div>
+      <!-- START THE FEATURETTES -->
 
-<!--- banner wrapper div end -->
-<div class="clearing"></div>
-<div class="container">
-
-
-  <div class="panel-wrapper">
-    @yield('contenido')
-    <div class="panel" style="width: 450px;">
       @yield('servicios')
-    </div>
 
-    <div class="panel" style="width: 100px;">
-      <div class="imgbg"></div>
-      <div class="content"></div>
-    </div>
+      <!-- /END THE FEATURETTES -->
 
 
-    <div class="panel">
-      @yield('beneficios')
-    </div>
-  </div>
-  <!--- panel wrapper div end -->
-</div>
-<!--- container div end -->
-<div class="clearing"></div>
-<div class="panel-wrapper">
-  <div class="panel marRight30">
-    @yield('contacto')
-  </div>
-  <div class="panel marRight30">
-    <div class="title">
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      </footer>
 
-    </div>
+    </div><!-- /.container -->
 
-    <div class="midcontent">
-      <div class="midcontentlink marRight40">
 
-    </div>
-  </div>
-  <div class="panel">
-    @yield('sociales')
-
-  </div>
-</div>
-
-    <p align="right" style="width: 1200px;" ><a href="login/login.php"><img src="images/admin.jpg"/></a></p>
-
-<div class="footer-wrapper">
-  <div class="footer">
-    <p> © 2017 All Rights Reserved.<a href="http://www.alltemplates.com"> < Computer and Services Networking Inc.  ></a> </p>
-  </div>
-</div>
-</body>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
 </html>
